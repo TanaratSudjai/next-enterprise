b8b1d61a12c23388944a3d3d52b13238e7a6999b
I'm Rick <149657689+TanaratSudjai@users.noreply.github.com>
2025-03-17 13:38:56 +0700

    deployed

--- components/Section/childSection/Content.tsx
+++ components/Section/childSection/Content.tsx
@@ -3,12 +3,14 @@ import React from "react"
 import Rescoures from "./Resources/Rescoures"
 import Alone from "./Resources/Alone"
 import Service from "./Resources/Service"
+import CardResoult from "./Resources/CardResoult"
 function Content() {
   return (
     <div className="mt-[-150px] bg-white">
       <Rescoures />
       <Alone />
       <Service />
+      <CardResoult />
     </div>
   )
 }
